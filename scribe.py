ee771d72e32302eea7480158deca64477d7ca77a
George Lundgren <georgesanders874@gmail.com>
2025-07-10 01:42:35 -0400

    scribe started

--- scribe.py
+++ scribe.py
@@ -1,4 +1,11 @@
 import argparse
+import os 
+import pretty_midi
+import librosa
+import torch
+from model.py import * 
+
+FREQ_BINS = 768
 
 def parse_args():
     parser = argparse.ArgumentParser(description="WAV to MIDI converter")
@@ -6,3 +13,30 @@ def parse_args():
     args = parser.parse_args()
 
     return args.wav_path
+
+def init_model(save_path): 
+    model = AudioToMidi
+
+def load_wav(file_name): 
+    pass
+
+def convert_midi(piano_roll): 
+    pass 
+
+def main(): 
+    #init model 
+    model = init_model()
+
+    #load file 
+    spec_array = load_wav(parse_args)
+
+    #transcribe
+    piano_roll = model(spec_array)
+
+    #convert to midi
+    midi = convert_midi(piano_roll)
+
+    #write out midi file
+
+if __name__ == "__main__": 
+    main()
