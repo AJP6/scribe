03a5162a0cb2a1c34caea759ff650b985d0831f6
TheClementX <georgesanders874@gmail.com>
2025-07-15 17:53:34 -0500

    code before size mismatch fix

--- data.py
+++ data.py
@@ -1,17 +1,29 @@
 import torch 
 import os
+import numpy as np
 from torch.utils.data import Dataset 
 
+MAX_LEN = 2048
 
-class SpectralData(Dataset, spec_dir, proll_dir): 
-    def __init__(self): 
+def pad_crop(x): 
+    if x.shape[-1] > MAX_LEN: 
+        return x[..., :MAX_LEN]
+    elif x.shape[-1] < MAX_LEN: 
+        pad_width = MAX_LEN - x.shape[-1]
+        return np.pad(x, ((0,0), (0, pad_width)), mode='constant')
+    else: 
+        return x
+
+
+class SpectralData(Dataset): 
+    def __init__(self, spec_dir, proll_dir): 
         self.spec_dir = spec_dir
         self.proll_dir = proll_dir
         self.pairs = []
 
         #list of spec and roll names paired 
-        spec = [f for f in os.listdir(spec_dir)]
-        roll = [f for f in os.listdir(proll_dir)]
+        spec = sorted([f for f in os.listdir(spec_dir)])
+        roll = sorted([f for f in os.listdir(proll_dir)])
         for s, r in zip(spec, roll): 
             self.pairs.append([s,r])
 
@@ -20,10 +32,10 @@ class SpectralData(Dataset, spec_dir, proll_dir):
 
     def __getitem__(self, idx): 
         spec_path = os.path.join(self.spec_dir, self.pairs[idx][0])
-        midi_path = os.path.join(self.spec_dir, self.pairs[idx][1])
+        midi_path = os.path.join(self.proll_dir, self.pairs[idx][1])
 
-        piano_roll = np.load(midi_path)
-        spec = np.load(spec_path)
+        piano_roll = pad_crop(np.load(midi_path))
+        spec = pad_crop(np.load(spec_path))
 
         #unsqueeze to add a dim for channels for the 
         #(bins, time_steps) --> (channels, bins, time_steps)
@@ -33,6 +45,6 @@ class SpectralData(Dataset, spec_dir, proll_dir):
         t_piano_roll = torch.tensor(piano_roll)
 
         #convert to float 32
-        return t_spec.float(), t_pianoroll.float()
+        return t_spec.float(), t_piano_roll.float()
         
 
--- train.py
+++ train.py
@@ -10,62 +10,75 @@ SPEC_DIR_TEST = '/home/clem3nti/projects/scribe/data/spectrograms_test'
 ROLL_DIR_TEST = '/home/clem3nti/projects/scribe/data/piano_rolls_test'
 
 EPOCHS = 60
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu' 
+#DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu' 
+DEVICE = 'cpu'
 FREQ_BINS = 96
+print("constants initialized")
 
-train_loader = DataLoader(SpectralData(SPEC_DIR_TRAIN, ROLL_DIR_TRAIN), batch_size=10, shuffle=True)
-test_loader = DataLoader(SpectralData(SPEC_DIR_TEST, ROLL_DIR_TEST), batch_size=10, shuffle=False)
+train_loader = DataLoader(SpectralData(SPEC_DIR_TRAIN, ROLL_DIR_TRAIN), batch_size=1, shuffle=True)
+test_loader = DataLoader(SpectralData(SPEC_DIR_TEST, ROLL_DIR_TEST), batch_size=1, shuffle=False)
+print("data loaders made")
+
 model = AudioToMidi(FREQ_BINS)
 model.to(DEVICE)
+print("model loaded")
 
 loss_fn = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+print("optimizer function loaded")
 
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+print("optimizer loaded")
+
+print("beginning training loop")
 for e in range(EPOCHS): 
+    i=0
     for spec_batch, roll_batch in train_loader: 
-        spec = spec_batch.to(device)
-        roll = roll_batch.to(device)
+        spec = spec_batch.to(DEVICE)
+        roll = roll_batch.to(DEVICE)
+        print(f"batch {i} loaded")
 
         preds = model(spec)
-        bin_preds = (preds > 0.5).float()
-        loss = loss_fn(bin_preds, roll_batch)
+        loss = loss_fn(preds, roll)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-    print(f"Epoch {e} : Loss = {loss.item():.4f}")
 
+        print(f"Loss = {loss.item():.4f}")
+        del spec, roll, preds, loss 
+        torch.cuda.empty_cache()
+        print(f"Batch: {i} of Epoch: {e} completed")
+        i+=1
+        
+    print(f"Epoch {e} completed") 
+
+
+print("eval loop beginning")
 model.eval()
-all_preds = []
-all_labels = []
+all_scores = []
 with torch.no_grad():
     for spec_batch, roll_batch in test_loader: 
-        spec = spec_batch.to(device)
-        roll = roll_batch.to(device)
+        spec = spec_batch.to(DEVICE)
+        roll = roll_batch.to(DEVICE)
 
         preds = model(spec)
         bin_preds = (preds > 0.5).float()
-        all_preds.append(bin_preds.cpu())
-        all_labels.append(roll_batch.cpu())
+
+        y_pred = preds.view(-1).cpu().numpy()
+        y_true = roll.view(-1).cpu().numpy()
         
-    all_preds = torch.cat(all_preds, dim=0)
-    all_labels = torch.cat(all_labels, dim=0)
+        cur_scores = dict() 
+        cur_scores["acc"] = accuracy_score(y_true, y_pred) 
+        cur_scores["prec"] = precision_score(y_true, y_pred, zero_division=0)
+        cur_scores["rec"] = recall_score(y_true, y_pred, zero_division=0)
+        cur_scores["f1"] = f1_score(y_true, y_pred, zero_division=0)
+        all_scores.append(cur_scores)
 
-    y_pred = all_preds.view(-1).numpy()
-    y_true = all_labels.view(-1).numpy()
-
+i = 0
+for score in all_scores: 
+    print(f"Score: {i}, Metrics: {all_scores[i]}")
+    i+=1
     
-    
-    acc = accuracy_score(y_true, y_pred) 
-    prec = precision_score(y_true, y_pred, zero_division=0)
-    rec = recall_score(y_true, y_pred, zero_division=0)
-    f1 = f1_score(y_true, y_pred, zero_division=0)
-
-    print(f"Accuracy: {acc:.4f}")
-    print(f"Precision: {prec:.4f}")
-    print(f"Recall: {rec:.4f}")
-    print(f"F1 Score: {f1:.4f}")
-
 torch.save(model.state_dict(), "model_states/model1.pth")
+print("model saved")
 
